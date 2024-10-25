e0deb23a63bc87f06da5283843992d8559fec599
nohahakim <raafatabass@hotmail.com>
2024-10-25 09:13:05 +0300

    fix

--- frontend/__tests__/RequestReset.test.js
+++ frontend/__tests__/RequestReset.test.js
@@ -1,1 +1,45 @@
-c;
+import { render, screen } from "@testing-library/react";
+import { MockedProvider } from "@apollo/client/testing";
+import userEvent from "@testing-library/user-event";
+import RequestReset, {
+  REQUEST_RESET_MUTATION,
+} from "../components/RequestReset";
+
+const email = "sara@example.com";
+const mocks = [
+  {
+    request: {
+      query: REQUEST_RESET_MUTATION,
+      variables: { email },
+    },
+    result: { data: { sendUserPasswordResetLink: null } },
+  },
+];
+
+describe("<RequestRest />", () => {
+  it("renders without crashing", () => {
+    const { container } = render(
+      <MockedProvider>
+        <RequestReset mocks={mocks} />
+      </MockedProvider>
+    );
+    expect(container).toMatchSnapshot();
+  });
+  it("allows the user to type an email", async () => {
+    render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <RequestReset />
+      </MockedProvider>
+    );
+    const emailInput = screen.getByPlaceholderText(/your email/i);
+    const signUpButton = screen.getByText(/request reset/i);
+    // Simulate typing here
+    await userEvent.type(emailInput, email);
+    expect(emailInput).toHaveValue(email);
+    await userEvent.click(signUpButton);
+    const successMessage = await screen.findByText(
+      `Success! Check your email for a link!`
+    );
+    expect(successMessage).toBeInTheDocument();
+  });
+});
