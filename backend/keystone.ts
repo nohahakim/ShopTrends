e22e231a72adb0ee06e9554f990a112abfa20f67
nohahakim <raafatabass@hotmail.com>
2025-01-27 21:50:19 +0200

    update8

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -28,7 +28,6 @@ const sessionConfig = {
 
   // secure: process.env.NODE_ENV === "production",
   // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-  secure: false,
 };
 
 const { withAuth } = createAuth({
