4d30510034e2fe0dbe5ae7aeefeb46ef60f76aa6
nohahakim <raafatabass@hotmail.com>
2025-01-27 07:08:06 +0200

    config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,13 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `http://localhost:3000/api/graphql`;
+// export const endpoint = `http://localhost:3000/api/graphql`;
 export const prodEndpoint = `https://shoptrends-pgkd.onrender.com/api/graphql`;
+// frontend/config.js
+
+export const endpoint =
+  process.env.NODE_ENV === "development"
+    ? `http://localhost:3000/api/graphql`
+    : `https://shoptrends-pgkd.onrender.com/api/graphql`;
+
+export const perPage = 2;
+
 export const perPage = 4;
