238bc03ac01907b94e189356c5b25a98df017e2b
nohahakim <raafatabass@hotmail.com>
2025-01-27 14:11:49 +0200

    keystone restore

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -25,9 +25,8 @@ const databaseURL =
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
   secret: process.env.COOKIE_SECRET,
-  sameSite: "None",
-  // secure: process.env.NODE_ENV === "production",
-  secure: true,
+  sameSite: "none",
+  secure: process.env.NODE_ENV === "production",
 };
 
 const { withAuth } = createAuth({
@@ -82,17 +81,9 @@ export default withAuth(
         // console.log(session);
         !!session?.data,
     },
-    session: withItemData(
-      statelessSessions({
-        ...sessionConfig,
-        // Explicitly set cookie properties
-        cookie: {
-          ...sessionConfig,
-          sameSite: 'None',
-          secure: true,
-        }
-      }),
-      {
-        User: `id name email role { ${permissionsList.join(" ")} }`,
-      }
-    )
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL Query
+      User: `id name email role { ${permissionsList.join(" ")} }`,
+    }),
+  })
+);
